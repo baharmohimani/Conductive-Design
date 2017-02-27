99b6f9bf3a7220f301707d63ae5d0e228e4fd40f
Brian Truong Nguyen <btn023@ucsd.edu>
2017-02-26 19:42:33 -0800

    Added some fixes for missing library, routing, and also added homepage stling

---
+++ public/js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+      $('.slider').slider();
+      $('.slider').height(450);
+      $('.slides').height('90%');
+
+      
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
